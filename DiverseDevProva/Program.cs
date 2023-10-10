8fa04daaf73b9faca0eec4a5198cdb9d7ef62c17
User <User@DESKTOP-QVSUCRK>
2023-10-10 18:39:17 -0300

    Adicionar arquivos de projeto.

---
+++ DiverseDevProva/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Data.SqlTypes;
+using System.Globalization;
+using System.Threading.Channels;
+
+namespace DiverseDev
+{
+    class Prova
+    {
+
+        static void Main(string[] args)
+        { 
+        
+         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+}
