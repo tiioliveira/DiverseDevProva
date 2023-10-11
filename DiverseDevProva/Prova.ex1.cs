a92c8af705a020abb2eb3d2865c42a49b7ab453e
Tiago Cruz <128756251+tiioliveira@users.noreply.github.com>
2023-10-10 21:26:08 -0300

    Add files via upload

---
+++ DiverseDevProva/Prova.ex1.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Data.SqlTypes;
+using System.Globalization;
+using System.Threading.Channels;
+
+namespace DiverseDev
+{
+    class Prova
+    {
+
+        static void Main(string[] args)
+        {
+                
+            int positivo = 0;
+            int negativo = 0;
+            int par = 0;
+            int impar = 0;
+            int j = 5;
+            int[] num = new int[j];
+
+
+
+            for (int i = 0; i < j; i++)
+                {
+                    Console.Write($"Digite o valor {i + 1}:");
+                    num[i] = int.Parse(Console.ReadLine());
+
+                    
+                    if (num[i] % 2 == 0)
+                    {
+                        par++;
+                    }
+                    else
+                    {
+                        impar++;
+                    }
+
+                    
+                    if (num[i] > 0)
+                    {
+                        positivo++;
+                    }
+                    else if (num[i] < 0)
+                    {
+                        negativo++;
+                    }
+                }
+
+
+            Console.WriteLine("\nValores positivos: " + positivo);
+            Console.WriteLine("\nValores negativos: " + negativo);
+            Console.WriteLine("\nValores pares: " + par);
+            Console.WriteLine("Valores ímpares: " + impar);
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+    }
---
+++ DiverseDevProva/Prova2.0.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Data.SqlTypes;
+using System.Globalization;
+using System.Threading.Channels;
+
+namespace DiverseDev
+{
+    class Prova
+    {
+
+        static void Main(string[] args)
+        {
+
+            int mult = 0;
+            int div = 0;
+            int n1;
+            int n2;
+               
+
+                Console.Write("Digite o primeiro número inteiro:");
+                n1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o segundo número inteiro:");
+                n2 = int.Parse(Console.ReadLine());
+
+                
+                for (int i = 0; i < Math.Abs(n2); i++)
+                {
+
+                    mult += Math.Abs(n1);
+
+                }
+
+                
+                int sinal = Math.Sign(n1) * Math.Sign(n2);
+
+
+                while (Math.Abs(n1) >= Math.Abs(n2))
+                {
+
+                    n1 = n1 - (sinal * n2);
+                    div++;
+
+                }
+
+                if ((n1 < 0 && n2 > 0) || (n1 > 0 && n2 < 0))
+                {
+
+                    div = -div;
+
+                }
+
+                 Console.WriteLine("Divisão Inteira: " + div);
+                 Console.WriteLine("Multiplicação: " + mult);              
+
+        }
+    }
+
+}
+
+
